b150ce72fd1278e334fad56a4473a0deec13ec1f
ya34ine <yassinemygt@gmail.com>
2026-08-10 12:26:06 +0100

    main

--- src/main/java/com/school/management/controller/StudentController.java
+++ src/main/java/com/school/management/controller/StudentController.java
@@ -2,6 +2,8 @@ package com.school.management.controller;
 
 import com.school.management.entity.Student;
 import com.school.management.repository.StudentRepository;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +24,50 @@ public class StudentController {
         return studentRepository.findAll();
     }
 
-    //test
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Long id){
+
+        return studentRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Student creaStudent(@RequestBody Student student){
+        return studentRepository.save(student);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(
+            @PathVariable Long id,
+            @RequestBody Student studentDetails) {
+
+        return studentRepository.findById(id)
+                .map(student -> {
+
+                    student.setFirstName(studentDetails.getFirstName());
+                    student.setLastName(studentDetails.getLastName());
+                    student.setEmail(studentDetails.getEmail());
+                    student.setClassName(studentDetails.getClassName());
+
+                    Student updatedStudent = studentRepository.save(student);
+
+                    return ResponseEntity.ok(updatedStudent);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+     @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
+
+        if (!studentRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        studentRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+    
 
 }
